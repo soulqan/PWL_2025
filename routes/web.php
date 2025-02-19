15eceb2b54e9a9f60f692963993062a28bf28d7b
soulqan <soultan.agnar@gmail.com>
2025-02-19 10:32:56 +0700

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,28 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
 
 // route1
-Route::get('/hello', function () {
-    return 'Hello World';
-   });
+// Route::get('/hello', function () {
+//     return 'Hello World';
+//    });
 
    Route::get('/world', function () {
     return 'World';
    });
 
-   Route::get('/', function () {
-    return 'Selamat Datang';
-   });
+//    Route::get('/', function () {
+//     return 'Selamat Datang';
+//    });
 
-   Route::get('/about', function () {
-    return 'NIM: 2341720191  NAMA: Soultan Mohammad Agnar Bisyarah';
-   });
+//    Route::get('/about', function () {
+//     return 'NIM: 2341720191  NAMA: Soultan Mohammad Agnar Bisyarah';
+//    });
 
    //Route 2
    Route::get('/user/{name}', function ($name) {
@@ -29,10 +34,10 @@ Route::get('/hello', function () {
  return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
 });
 
-Route::get('/articles/{id}', function
-($id) {
- return 'Halaman Artikel dengan ID '.$id;
-});
+// Route::get('/articles/{id}', function
+// ($id) {
+//  return 'Halaman Artikel dengan ID '.$id;
+// });
 
 
 //route 3
@@ -78,3 +83,22 @@ Route::get('/user/{name?}', function ($name='John') {
         Route::redirect('/here', '/there');
         Route::view('/welcome', 'welcome');
         Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+        // controller
+        Route::get('/hello', [WelcomeController::class,'hello']);
+        
+        Route::get('/', [HomeController::class,'index']);
+
+        Route::get('/about', [AboutController::class,'about']);
+
+        Route::get('/articles/{id}', [ArticleController::class,'articles']);
+       
+        
+        // Route::resource('photos', PhotoController::class);
+
+        Route::resource('photos', PhotoController::class)->only([
+            'index', 'show'
+           ]);
+           Route::resource('photos', PhotoController::class)->except([
+            'create', 'store', 'update', 'destroy'
+           ]);
