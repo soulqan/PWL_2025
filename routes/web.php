a16d7989d3319834aca525f5c2248f8e4c7397b6
soulqan <soultan.agnar@gmail.com>
2025-02-19 09:28:16 +0700

    route1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/hello', function () {
+    return 'Hello World';
+   });
+
+   Route::get('/world', function () {
+    return 'World';
+   });
+
+   Route::get('/', function () {
+    return 'Selamat Datang';
+   });
+
+   Route::get('/about', function () {
+    return 'NIM: 2341720191  NAMA: Soultan Mohammad Agnar Bisyarah';
+   });
